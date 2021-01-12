8e6907a26205c30fe5e772e6a121726d24a794b9
IvanHdzRz <musedelick70@gmail.com>
2021-01-12 17:00:03 -0600

    se añadiero pagina de index -se añadieron estilos a la pagina index

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!--Fuentes -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;200;300;400;500;700&display=swap" rel="stylesheet">
+    <!--Main css-->
+    <link rel="stylesheet" href="main.css">
+    <title>Ing. Conocimiento</title>
+</head>
+<body>
+    <nav>
+        <p>Dominguez Hernandez Rodney Axel</p>
+    </nav>
+    <div class="hero">
+        <div class="hero_title">
+            <h2>Proyecto para la asignatura ingenieria del conocimiento </h2>
+        </div>
+        <div class="hero_paragraph">
+            <p>
+                Aplicaciones de los fundamentos de la inteligencia articial haciendo uso de
+                operaciones de clasificacion y agrupacion
+            </p>
+        </div>
+        <div class="hero_links">
+            <a href="#" class="btn principal">Agrupador</a>
+            <a href="#" class="btn principal" >Clasificador</a>
+        </div>
+    </div>
+</body>
+</html>
